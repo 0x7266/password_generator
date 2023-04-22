38827cced796384569235752ef5481e046f4e8f7
rafael <rrrr0ffff@gmail.com>
2023-04-22 01:01:40 -0300

    check if at least one of the options is checked

---
+++ index.js
@@ -0,0 +1,32 @@
+const output = document.getElementById("pwd");
+const copy = document.getElementById("copy");
+const form = document.getElementById("form");
+const length = document.getElementById("length");
+const lengthOutput = document.getElementById("length_output");
+const uppercase = document.getElementById("uppercase");
+const lowercase = document.getElementById("lowercase");
+const numbersOption = document.getElementById("numbers");
+const symbolsOption = document.getElementById("symbols");
+const generate = document.getElementById("generate");
+
+const SYMBOLS = "_-¿!@#$%^&*()?¡".split("");
+const NUMBERS = "0123456789".split("");
+const LOWERCASE_LETTERS = "abcdefghijklmnopqrstuvwxyz".split("");
+const UPPERCASE_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+
+function generatePassword(e) {
+	e.preventDefault();
+	// at least one of the options must be checked
+	if (
+		![
+			lowercase.checked,
+			uppercase.checked,
+			numbersOption.checked,
+			symbolsOption.checked,
+		].some((element) => element)
+	) {
+		alert("Password"); // TODO: SHOW ERROR MESSAGE
+	}
+}
+
+form.addEventListener("submit", generatePassword);
