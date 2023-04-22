67862d2a778591e4dad99081c75cb5e69161570d
rafael <rrrr0ffff@gmail.com>
2023-04-22 02:17:57 -0300

    user cant uncheck last checkbox

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const copy = document.getElementById("copy");
 const form = document.getElementById("form");
 const length = document.getElementById("length");
 const lengthOutput = document.getElementById("length_output");
+const strengthOutput = document.getElementById("strength_output");
 const uppercase = document.getElementById("uppercase");
 const lowercase = document.getElementById("lowercase");
 const numbers = document.getElementById("numbers");
@@ -16,21 +17,6 @@ const UPPERCASE_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 function generatePassword(e) {
 	e.preventDefault();
-	// at least one of the options must be checked
-	if (
-		![
-			lowercase.checked,
-			uppercase.checked,
-			numbers.checked,
-			symbols.checked,
-		].some((element) => element)
-	) {
-		passwordOutput.style.color = "#62616a";
-		passwordOutput.innerText = "P4$5W0rD!";
-		// TODO: SHOW ERROR MESSAGE
-		alert("Password");
-		return;
-	}
 	let password = [
 		...(symbols.checked ? SYMBOLS : []),
 		...(numbers.checked ? NUMBERS : []),
@@ -46,10 +32,55 @@ function generatePassword(e) {
 		.slice(0, Number(length.value));
 	passwordOutput.style.color = "white";
 	passwordOutput.innerText = password;
+	checkStrength();
 }
+
+function checkStrength() {
+	let options = [
+		lowercase.checked,
+		uppercase.checked,
+		numbers.checked,
+		symbols.checked,
+	].filter((element) => Boolean(element)).length;
+	let score =
+		lowercase.checked + uppercase.checked + numbers.checked + symbols.checked;
+	console.log(score);
+	if (length.value >= 12) score += 7;
+	else if (length.value >= 8) score += 5;
+	else if (length.value >= 6) score += 2;
+	else length += 1;
+	score = Math.floor(score / 2);
+	const descriptions = {
+		1: "Very weak",
+		2: "Weak",
+		3: "Moderate",
+		4: "Strong",
+		5: "Very Strong",
+	};
+	strengthOutput.innerText = descriptions[score];
+}
+
+document.querySelectorAll("input[type=checkbox]").forEach((i) =>
+	i.addEventListener("change", (e) => {
+		// at least one of the options must be checked
+		if (
+			![
+				lowercase.checked,
+				uppercase.checked,
+				numbers.checked,
+				symbols.checked,
+			].some((element) => element)
+		) {
+			e.target.checked = true;
+			return;
+		}
+		checkStrength();
+	})
+);
 
 length.addEventListener("input", (e) => {
 	lengthOutput.innerText = e.target.value;
+	checkStrength();
 });
 
 form.addEventListener("submit", generatePassword);
